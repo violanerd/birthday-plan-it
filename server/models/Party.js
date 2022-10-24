9a3aae4a8e2f64c33867292fa997b8c832e0d8b1
Josh <josh.haddix598@gmail.com>
2022-10-23 21:09:41 -0500

    initial model framework

---
+++ server/models/Party.js
@@ -0,0 +1,33 @@
+import { Schema } from "mongoose";
+
+const partySchema = new Schema(
+  {
+    user: {
+      type: String,
+      required: true,
+    },
+    guests: {
+      // placeholder
+    },
+    date: {
+      type: Date,
+    },
+    location: {
+      type: String,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    theme: {},
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {}
+);
+
+const Party = model("Party", partySchema);
+
+module.exports = Party;
---
+++ server/models/User.js
@@ -0,0 +1,27 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  parties: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Party",
+    },
+  ],
+});
