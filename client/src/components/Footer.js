7829f2145b38f98c0a4de61fe205701d49de0436
Ron Dronet <ron@dronetdevdesign.com>
2022-10-26 11:09:24 -0500

    creating footer component containing logo and app credits with copyright

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-import './components/FooterStyles.css';
+import './FooterStyles.css';
+import { Link } from 'react-router-dom';
+import Logo from './assets/birthday-plan-it-logo.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='footer-container'>
+        <Link to={'/'}>
+          <div className='bpi-logo'>
+            <img src={Logo} alt="logo" className='birthday-plan-it-logo' />
+          </div>
+        </Link>
+        <p className='credits'>This web application was coded and created by: Madalyne Cross, Joshua Maddox and Ron Dronet</p>
+        <p>&copy; Copyright 2022</p>
+      </div>
+    </div>
   )
 }
 
