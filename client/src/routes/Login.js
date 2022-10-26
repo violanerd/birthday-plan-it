1236c77053f1692e3a441d067e894817909eacff
Ron Dronet <ron@dronetdevdesign.com>
2022-10-26 11:49:41 -0500

    creating footer component containing logo and app credits with copyright

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+  return <div>
+    <Navbar />
+    <Footer />
+  </div>
 }
 
 export default Login;
--- client/src/routes/MyParty.js
+++ client/src/routes/MyParty.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 const MyParty = () => {
-  return (
-    <div>MyParty</div>
-  )
+  return <div>
+    <Navbar />
+    <Footer />
+  </div>
 }
 
 export default MyParty;
