1434841524118799219c1b521f4d436656e9288f
Josh <josh.haddix598@gmail.com>
2022-10-26 20:58:06 -0500

    Signup and Login created

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,46 +1,63 @@
-import './NavbarStyles.css';
-
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Logo from './assets/birthday-plan-it-logo.png';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import "./NavbarStyles.css";
+import SignUp from "./SignUpModal";
+import Login from "./LoginModal";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "./assets/birthday-plan-it-logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   // ACTIVATE SLIDE IN MENU BY 'click' STATE
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
 
+  const [isSignUpOpen, setIsSignUpOpen] = useState(false);
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+
+  function handleModals(modal) {
+    if (modal === "login") {
+      setIsSignUpOpen(false);
+      setIsLoginOpen(!isLoginOpen);
+    }
+    if (modal === "signup") {
+      setIsLoginOpen(false);
+      setIsSignUpOpen(!isSignUpOpen);
+    }
+  }
+
   return (
-    <div className='header'>
-      <Link to={'/'}>
-        <div className='logo-section'>
-          <img src={Logo} alt="logo" className='birthday-plan-it-logo' />
+    <div className="header">
+      {isSignUpOpen && <SignUp handleModals={handleModals}></SignUp>}
+      {isLoginOpen && <Login handleModals={handleModals}></Login>}
+      <Link to={"/"}>
+        <div className="logo-section">
+          <img src={Logo} alt="logo" className="birthday-plan-it-logo" />
         </div>
       </Link>
-      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li>
-          <Link to='/'>Home</Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to='/myparty'>My Party</Link>
+          <Link to="/myparty">My Party</Link>
         </li>
         <li>
-          <Link to='/signup'>Sign Up</Link>
+          <Link onClick={() => handleModals("signup")}> Sign Up</Link>
         </li>
-        <li className='login-btn'>
-          <Link to='/login'>Login</Link>
+        <li className="login-btn">
+          <Link onClick={() => handleModals("login")}>Login</Link>
         </li>
       </ul>
       {/* HAMBURGER MENU CONDITIONALS */}
-      <div className='hamburger' onClick={handleClick}>
+      <div className="hamburger" onClick={handleClick}>
         {click ? (
-          <FaTimes size={25} style={{ color: '#fff' }} />
+          <FaTimes size={25} style={{ color: "#fff" }} />
         ) : (
-          <FaBars size={25} style={{ color: '#ffffff' }} />
+          <FaBars size={25} style={{ color: "#ffffff" }} />
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
