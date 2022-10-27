e9010efaadf581e9ee3de7a5fd5a4c441e2fe37b
Ron Dronet <ron@dronetdevdesign.com>
2022-10-27 13:15:33 -0500

    created the party form template for PartyForm.js

--- client/src/components/PartyForm.js
+++ client/src/components/PartyForm.js
@@ -5,42 +5,45 @@ import ThemeThree from './assets/theme03.jpg';
 
 function PartyForm() {
     return (
-        <div className='theme-container-form'>
-            <div className='form-field-container'>
-                <form className='form' onSubmit=''>
-                    <img className='theme-three' src={ThemeThree} alt='dance-party-theme' />
-                    <div className='input-fields'>
-                        <p>You're invited to a party!</p>
-                        <label htmlFor='name'></label>
-                        <input type='text' name='name' placeholder='Your name goes here'/>
-                        <p>Would like to invite you to her birthday party!</p>
-                        <div>
-                            <label htmlFor='host'>Who is hosting the party?</label>
-                            <input type='text' name='host' />
+        <div className='background-container'>
+            <div className='theme-container-form'>
+                <div className='form-field-container'>
+                    <form className='form' onSubmit=''>
+                        <img className='theme-three' src={ThemeThree} alt='dance-party-theme' />
+                        <div className='input-fields'>
+                            <label className='you-r-invited' htmlFor='name'>YOU'RE INVITED!</label>
+                            <input className='host-name' type='text' name='name' placeholder='Your name goes here' />
+                            <p className='label label-p' >would like to invite you to their birthday party!</p>
+                            <label className='label' htmlFor='location'>The party will be held at</label>
+                            <textarea placeholder="Your address"
+                                defaultValue="Hello!"
+                                className="location"
+                                rows='2'
+                                maxlength='100'
+                                name='location'>
+                                </textarea>
+                            <label className='label' htmlFor="date">The date of the party will be</label>
+                            <div className='date-time'>
+                            <input className='date' type='date' name='date' />
+                            </div>
+                            <label className='label' htmlFor='time'>The party will start at</label>
+                            <div className='date-time'>
+                            <input className='time' type='time' name='time' />
+                            </div>
+                            {/* <textarea
+                            placeholder="Message to your guests"
+                            value=""
+                            className=""
+                            onChange=""
+                            name="description"
+                            max-length='5'
+                        ></textarea> */}
+                            <button className='create-invite-btn' type='submit'>
+                                Create Invitation
+                            </button>
                         </div>
-                        <div>
-                            <label htmlFor="date">Pick the party date:</label>
-                            <input type='date' name='date' />
-                        </div>
-                        <div>
-                            <label htmlFor='location'>Where will the party be?</label>
-                            <input type='text' name='location' />
-                        </div>
-                        <div>
-                            <textarea
-                                placeholder="Describe the party..."
-                                value=""
-                                className=""
-                                onChange=""
-                                name="description"
-                                max-length='5'
-                            ></textarea>
-                        </div>
-                        <button className='' type='submit'>
-                            Create My Invitation
-                        </button>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     )
