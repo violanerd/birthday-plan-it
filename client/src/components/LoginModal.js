521c5912312f909d4e529c8b8eb703b9aede37c7
Josh <josh.haddix598@gmail.com>
2022-10-27 14:12:18 -0500

    Front end signup / login functionality

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,16 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ModalStyles.css";
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "../utils/mutations";
+import Auth from "../utils/auth";
+
 function Login({ handleModals }) {
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error }] = useMutation(LOGIN);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+      handleModals("login");
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <div className="modalBackdrop">
       <div className="modalContainer">
         <h3 className="modalTitle">Login!</h3>
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="email">Email Address:</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            value={formState.email}
+            onChange={handleChange}
+          />
 
-        <label htmlFor="email">Email Address:</label>
-        <input type="email" name="email" />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="********"
+            value={formState.password}
+            onChange={handleChange}
+          />
+          <button type="submit">Login!</button>
+        </form>
 
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" />
+        {error && <div>Login failed</div>}
 
         <div className="flex-row">
           <button type="button" onClick={() => handleModals("signup")}>
--- client/src/components/SignUpModal.js
+++ client/src/components/SignUpModal.js
@@ -1,18 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ModalStyles.css";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
+
 function SignUp({ handleModals }) {
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+      handleModals("signup");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div className="modalBackdrop">
       <div className="modalContainer">
         <h3 className="modalTitle">Sign Up!</h3>
-        <label htmlFor="username">Username:</label>
-        <input type="text" name="username" />
 
-        <label htmlFor="email">Email Address:</label>
-        <input type="email" name="email" />
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Your Username"
+            value={formState.username}
+            onChange={handleChange}
+          />
 
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            value={formState.email}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="********"
+            value={formState.password}
+            onChange={handleChange}
+          />
+
+          <button type="submit">Sign Up!</button>
+        </form>
+
+        {error && <div>Sign Up Failed</div>}
 
         <div className="flex-row">
           <button type="button" onClick={() => handleModals("signup")}>
