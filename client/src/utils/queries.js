fae03201bda01675a99a3e9575e6b07264683086
violanerd <madalyne.cross@gmail.com>
2022-10-27 09:53:43 -0500

    mutations and queries added on client

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -6,4 +6,67 @@ query emailGuests($id: ID!) {
       guests
     }
   }
-`
+`;
+
+export const QUERY_ME = gql`
+query me {
+  me {
+    _id
+    username
+    email
+    parties {
+      _id
+      name
+      description
+      host
+      guests
+      date
+      location
+      theme
+    }
+  }
+}`
+export const QUERY_USER = gql`
+query User($username: String) {
+  user(username: $username) {
+    _id
+    username
+    email
+    parties {
+      _id
+      name
+      description
+      host
+      guests
+      date
+      location
+      theme
+    }
+  }
+}`
+export const QUERY_PARTIES = gql`
+query Parties($host: String) {
+  parties(host: $host) {
+    _id
+    name
+    description
+    guests
+    host
+    date
+    location
+    theme
+  }
+}`
+export const QUERY_PARTY = gql`
+query Party($id: ID!) {
+  party(_id: $id) {
+    _id
+    name
+    description
+    host
+    guests
+    date
+    location
+    theme
+  }
+}`
