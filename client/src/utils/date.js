532dbebac8db12ac288ffc93cf19654975f21507
violanerd <madalyne.cross@gmail.com>
2022-11-02 12:48:34 -0500

    time edge cases

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -3,26 +3,31 @@ function dateFormat(dateVal) {
         return "Uh oh, no date selected!"
     } else {
         let toInt = parseInt(dateVal)
-    let newDate = new Date(toInt);
-    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
+        let newDate = new Date(toInt);
+        let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC'}
     return newDate.toLocaleDateString('en-us', options)
     }
 }
 
 function parseTime(time) {
+    console.log(time)
     if (!time){
         return "whenever you arrive!"
     } else {
         let hour = parseInt(time);
         let minutes = time.substring(3,5);
 
-        if(time > '12:00') {
-            return `${hour - 12}:${minutes} PM`;
-        } else {
+        //edge case
+        if (time==='12:00') {
+            return `${hour}:${minutes} PM`
+        } else if (time==='00:00') {
+            return "12:00 AM"
+        } else if(time > '12:00') {
+            return `${hour - 12}:${minutes} PM`
+        }  else {
             return `${hour}:${minutes} AM`;
         }
     }    
 }
 
-// thanks https://stackoverflow.com/questions/17884586/javascript-parsing-times-without-date
 module.exports = { dateFormat, parseTime }
